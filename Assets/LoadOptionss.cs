9d9144e026c1b2ffeef3373aefb8826f684e0541
Kristijan200909 <filipovskikristijan23@gmail.com>
2025-11-30 18:42:24 +0100

    d

---
+++ Assets/LoadOptionss.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadOptions : MonoBehaviour
+{
+    public void LoadOptionsScene()
+    {
+        SceneManager.LoadScene("Options");
+    }
+}
