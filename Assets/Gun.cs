280d52becf83427be0c09e1d5c5c06bf6f627f87
Praktikanti 4-5 <praktikanti35@gmail.com>
2025-12-24 17:12:49 +0100

    asc

--- Assets/Gun.cs
+++ Assets/Gun.cs
@@ -5,7 +5,7 @@ public class Gun : MonoBehaviour
 
     public float damage = 10f;
     public float range = 100f;
-    public float firerate = 50f;
+    public float firerate = 15f;
 
 
     public Camera fpsCam;
