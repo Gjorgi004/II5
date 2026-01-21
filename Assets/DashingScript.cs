230acff1a60ca394b90ea9cc30b10f6e0b0f01df
Praktikanti 4-5 <praktikanti35@gmail.com>
2026-01-21 11:35:24 +0100

    mapi
    d

---
+++ Assets/DashingScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class DashingScript : MonoBehaviour
+{
+    //public CharacterController controller;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+  //  If (Input.getKeyDown(KeyCode.LeftShift))
+  //  {
+   //     controller.Move(transform.forward * 10f * Time.deltaTime);
+  //  }
+
+}
