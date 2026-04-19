3a49e90a1f19e506efa892bff6de42b5403cacc9
Gjorgi004 <tasevskigjorgi077@gmail.com>
2026-04-19 16:54:56 +0200

    g

---
+++ Assets/SCRIPTS/EquipmentSlot.cs
@@ -0,0 +1,13 @@
+
+public class EquipmentSlot : ItemSlot
+{
+
+    public EquipmentType EquipmentType;
+
+    protected override void OnValidate()
+    {
+        base.OnValidate();
+        gameObject.name = EquipmentType.ToString() + " Slot";
+    }
+
+}
